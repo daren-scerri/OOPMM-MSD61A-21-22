1c17bd320204f885c682d3e29e675c59b6bfe52a
daren-scerri <daren.scerri@mcast.edu.mt>
2021-11-01 21:01:39 +0100

    Worksheet 2 and 3 solutions

---
+++ Worksheet2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Enemy
+{
+    public int hitpoints, damage;
+    string name;
+
+    public bool enemydead;
+
+    private GameObject enemyprefab, enemy;
+
+    public Enemy(int hp, int dmg, string ID, bool hasdied, GameObject enemyprefab)
+    { //this constructor assigns hitpoints, damage, and name to
+      //the values passed into the constructor
+        hitpoints = hp;
+        damage = dmg;
+        name = ID;
+        enemydead = hasdied;
+        enemy = GameObject.Instantiate(enemyprefab, new Vector3(Random.Range(-5f, +5f), 0f, 0f), Quaternion.identity);
+        enemy.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+
+
+    public void TakeDamage()
+    {
+        if (hitpoints > 0)
+        {
+            hitpoints--; //reduce HP by 1
+            Debug.Log(name + "'s HP: " + hitpoints); //print out new hp
+        }
+    }
+
+
+    public void Die()
+    {
+        enemydead = true;
+        Debug.Log(name + " Has Died"); //print to the console
+        GameObject.Destroy(enemy);
+    }
+
+
+
+}
---
+++ Worksheet3/Assets/Scripts/Bouncer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bouncer : MonoBehaviour
+{
+    
+    [SerializeField] int ballhealth = 100;
+    Rigidbody2D rb;
+
+
+    [SerializeField] Text mytext;
+
+
+
+    Vector2 otherVelocity = new Vector2(0f, 0f);
+  //  [SerializeField] GameObject otherObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(400f,200f));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        ballhealth-=10;
+
+        mytext.text = "Health: " + ballhealth.ToString();
+
+        otherVelocity= otherVelocity + new Vector2(50f, 50f);
+        Debug.Log(ballhealth);
+         Color tmp = GetComponent<SpriteRenderer>().color;
+         tmp.a-= 0.1f;
+         GetComponent<SpriteRenderer>().color = tmp;
+         if (ballhealth<=0)
+            {
+            mytext.text = "THE BALL IS DEAD";
+            Destroy(this.gameObject);
+            }
+            
+    }
+
+}
